d7c4b1d769b8a26be5b3496de4f6089d84f75067
Johan_Trieloff <johan@trieloff.net>
2023-07-28 11:11:05 +0200

    added right arrow

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,3 +1,15 @@
 export default function decorate(block) {
     console.log('decorate accordion');
+
+    // Create a new span element
+    const span = document.createElement('span');
+    span.className = 'icon-arrow';  // Set a class for styling in CSS
+
+    // Find all question divs
+    const questions = block.querySelectorAll('.accordion.block > div > div:nth-child(1)');
+
+    // Append the span (which will contain the SVG via CSS) to each question
+    questions.forEach(question => {
+        question.appendChild(span.cloneNode(true));
+    });
 }
