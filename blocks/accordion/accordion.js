3c732990536f2df9cd4c9f50b4ef9a3300b4aaea
Johan_Trieloff <johan@trieloff.net>
2023-07-28 11:34:05 +0200

    basic handleClick

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,15 +1,38 @@
-export default function decorate(block) {
-    console.log('decorate accordion');
+function handleClick(event) {
+    console.log('handleClick()');
+    
+    let target = event.target;
+    
+    // if the target is span, change it to its parent (the question div)
+    if (target.nodeName === 'SPAN') {
+        target = target.parentNode;
+    }
+    
+    const parentDiv = target.parentNode;
+    const answerDiv = parentDiv.querySelector(':nth-child(2)');
+    
+    // get the computed style of the answer div
+    const computedStyle = window.getComputedStyle(answerDiv);
+    
+    if (computedStyle.display === "none") {
+        console.log('showing answer');
+        answerDiv.style.display = "block";
+    } else {
+        console.log('hiding answer');
+        answerDiv.style.display = "none";
+    }
+}
 
-    // Create a new span element
+
+export default function decorate(block) {
     const span = document.createElement('span');
     span.className = 'icon-arrow';  // Set a class for styling in CSS
 
-    // Find all question divs
     const questions = block.querySelectorAll('.accordion.block > div > div:nth-child(1)');
 
     // Append the span (which will contain the SVG via CSS) to each question
     questions.forEach(question => {
         question.appendChild(span.cloneNode(true));
+        question.addEventListener('click', handleClick);
     });
 }
