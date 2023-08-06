2b1190c01082b695c605e7b44461c1ca8ca1978c
Johan Trieloff <johan@trieloff.net>
2023-08-06 20:35:03 +0200

    discord social proof done

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -6,12 +6,28 @@ export default function decorate(block) {
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     li.innerHTML = row.innerHTML;
+
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        div.className = 'cards-card-image';
+      } else {
+        div.className = 'cards-card-body';
+
+        // Wrap the first three children of cards-card-body in a wrapper div
+        const wrapper = document.createElement('div');
+        wrapper.className = 'top-elements';
+        for (let i = 0; i < 3 && div.children.length > 0; i++) {
+          wrapper.append(div.children[0]); // move first child to wrapper
+        }
+        if (wrapper.children.length > 0) {
+          div.insertBefore(wrapper, div.firstChild);
+        }
+      }
     });
+
     ul.append(li);
   });
+
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
