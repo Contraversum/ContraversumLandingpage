fa4ce05e49dcc677c441f4c71c602cd7118d3c79
Johan Trieloff <johan@trieloff.net>
2023-08-12 15:28:29 +0200

    steps v1 done

--- blocks/steps/steps.js
+++ blocks/steps/steps.js
@@ -15,6 +15,33 @@ function wrapTextinDiv() {
     });
 }
 
+function addLines() {
+    // Select all child divs of .steps.block
+    const steps = document.querySelectorAll('.steps.block > div');
+    
+    // Loop through each div and check its index
+    steps.forEach((step, index) => {
+        // If this is the last div, don't append anything
+        if (index === steps.length - 1) {
+            return;
+        }
+        
+        // Create an img element for the SVG
+        const svgImg = document.createElement('img');
+
+        // Depending on the index, set the src for the img
+        if (index % 2 === 0) { // odd index (0-based)
+            svgImg.src = "/images/even-line.svg";
+        } else { // even index (0-based)
+            svgImg.src = "/images/uneven-line.svg";
+        }
+
+        // Append the SVG img after the current div
+        step.parentNode.insertBefore(svgImg, step.nextSibling);
+    });
+}
+
 export default function decorate(block) {
     wrapTextinDiv();
+    addLines();
 }
