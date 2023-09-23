d5529498dbeb4eef7bece7f69e0bfb9fc38e35e2
Johan Trieloff <johan@trieloff.net>
2023-09-23 18:55:40 +0200

    styled button

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,4 +1,4 @@
-import { getMetadata, decorateIcons } from '../../scripts/lib-franklin.js';
+import { getMetadata, decorateIcons, decorateButtons } from '../../scripts/lib-franklin.js';
 
 // media query match that indicates mobile/tablet width
 const isDesktop = window.matchMedia('(min-width: 900px)');
@@ -138,6 +138,7 @@ export default async function decorate(block) {
         isDesktop.addEventListener('change', () => toggleMenu(nav, navSections, isDesktop.matches));
 
         decorateIcons(nav);
+        decorateButtons(nav);
         const navWrapper = document.createElement('div');
         navWrapper.className = 'nav-wrapper';
         navWrapper.append(nav);
